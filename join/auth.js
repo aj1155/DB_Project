bdea1996df127dc822ae646cc4f0a1b8cc51db25
JHJG <kangjungu1@KangJunguui-MacBook-Pro.local>
2016-11-10 21:28:15 +0900

    로그인 및 비밀번호 변경 완료

--- join/auth.js
+++ join/auth.js
@@ -15,8 +15,8 @@
  * @version 1.0, 2016.11.06 주석 추가
  * @see    None
  */
-var mysql=require('mysql');
-var pool=require('./connection'); // 생성 했던 dbPool을 가져온다
+var mysql = require('mysql');
+var pool = require('./connection'); // 생성 했던 dbPool을 가져온다
 
 /*이 부분은 모바일을 위해 만든 Outh2.0 관련 부분인데 아직은  알 필요 없다 */
 var jwt = require('jsonwebtoken');
@@ -26,14 +26,16 @@ var EXPIRES = 60; // 1 hour
 var validateJwt = require('express-jwt')({secret: SECRET});
 
 /*해당 비밀 번호의 User 정보를 가져오는 부분*/
-function UserAuth(login_id,category_id,callback){
-  pool.getConnection(function(err,connection){
-    connection.query("SELECT id,login_id,password,category_id FROM user where login_id=? && category_id=?",[login_id,category_id],function(err, row){
-      callback(row[0]);
-      connection.release();
-
+function UserAuth(login_id, category_id, callback) {
+    pool.getConnection(function (err, connection) {
+        if(err){
+            console.error("error is " + err);
+        }
+        connection.query("SELECT id,login_id,password,category_id FROM user where login_id=? && category_id=?", [login_id, category_id], function (err, row) {
+            callback(row[0]);
+            connection.release();
+        });
     });
-  });
 
 
 }
@@ -47,25 +49,25 @@ function signToken(id) {
 
 // 토큰을 해석하여 유저 정보를 얻는 함수
 function isAuthenticated() {
-  return compose()
-      // Validate jwt
-      .use(function(req, res, next) {
-        // 만약 access_token 파라메터에 토큰을 설정한 경우 리퀘슽 헤더에 토큰을 설정한다.
-        if(req.query && req.query.hasOwnProperty('access_token')) {
-          req.headers.authorization = 'Bearer ' + req.query.access_token;
-        }
+    return compose()
+    // Validate jwt
+        .use(function (req, res, next) {
+            // 만약 access_token 파라메터에 토큰을 설정한 경우 리퀘슽 헤더에 토큰을 설정한다.
+            if (req.query && req.query.hasOwnProperty('access_token')) {
+                req.headers.authorization = 'Bearer ' + req.query.access_token;
+            }
 
-        // 토큰 인증 로직
-        validateJwt(req, res, next);
-      })
-      // Attach user to request
-      .use(function(req, res, next) {
-        req.user = {
-          id: req.user.id,
-          name: 'name of ' + req.user.id
-        };
-        next();
-      });
+            // 토큰 인증 로직
+            validateJwt(req, res, next);
+        })
+        // Attach user to request
+        .use(function (req, res, next) {
+            req.user = {
+                id: req.user.id,
+                name: 'name of ' + req.user.id
+            };
+            next();
+        });
 
 }
 
