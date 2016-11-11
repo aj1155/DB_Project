8554245c9de91d7d303f171305132b05289f078c
cheoljin <kcj091857@gmail.com>
2016-11-11 19:13:28 +0900

    라우트 설정해주고 회원 관리 view 다시 만들어서 올림

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -12,6 +12,9 @@ router.get('/guide', function(req, res, next) {
   res.render('admin/guide');
 });
 
-
+//작성자 : 강철진 11/11 내용 :user 추가 편집 삭제 라우트설정
+router.get('/userManage', function(req, res ,next) {
+  res.render('admin/userManage');
+});
 
 module.exports = router;
