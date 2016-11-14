14cf4cf10f6e06f2ec3478b0f5687310e1a1f485
sumusb@naver.com <sumusb@naver.com>
2016-11-14 20:54:27 +0900

    게시판 목록이랑 ,조회부분이야 아직 오류가 좀많아서 수정해서 계속 올릴께

---
+++ routes/board/board.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var boardDAO = require('../../query/board/board');
+var pool = require('../../join/connection');
+
+router.get('/list',function(req,res,next){
+  boardDAO.selectAll(function(rows){
+    res.render('board/list',{
+      row:rows
+    })
+  });
+});
+
+router.get('/read/:id',function(req,res,next){
+  boardDAO.selectById([req.params.id],function(rows){
+    res.render('board/read',{
+      rows:rows
+    })
+  });
+});
+
+router.get('/write',function(req,res,next){
+  res.render('board/write');
+});
+
+router.post('/write',function(req,res,next){
+  boardDAO.board_insert([req.body.title,req.body.content],function(result){
+    if(result){
+      console.log('success');
+      res.redirect('board/list');
+    }
+  });
+});
+module.exports = router;
