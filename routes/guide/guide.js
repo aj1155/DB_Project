2269209f20904b38caddd2f55c86fd12e45809d7
aj1155 <aj1155@naver.com>
2016-11-18 11:11:22 +0900

    [#4] adming introduce

--- routes/guide/guide.js
+++ routes/guide/guide.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var Introduce = require('../../model/Introduce');
+var sequelize = require('../../join/sequelize'); /* node.js orm sequelize 설정을 불러옴 */
 
 /* GET home page. */
 router.get('/introduce', function(req, res, next) {
-  res.render('guide/introduce');
+  sequelize.authenticate().then(function(err){
+    Introduce.findAll({
+      where : {
+        id : req.user.category_id
+      }
+    })
+    .then(function(rows){
+      res.render('guide/introduce',{body:rows[0].text});
+    });
+  })
+  .catch(function(err){
+    res.send(err);
+  });
 });
+
 
 
 
