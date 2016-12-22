266c939816a7044094e617ee832379c211ff9fd3
cheoljin <kcj091857@gmail.com>
2016-12-22 13:31:53 +0900

    admin manager부분 수정

--- public/res/assets/js/admin/userManage.js
+++ public/res/assets/js/admin/userManage.js
@@ -41,7 +41,7 @@ $(function(){
                     '<tr onClick="location.href=\'/admin/userEdit/'+row.id+'\'" style="cursor:pointer;">'+
                     '<td>'+
                         '<div class="icheckbox_flat-green" style="position: relative;">'+
-                          '<input type="checkbox" class="flat" name="table_records" value="'+row.id+'" style="position: relative; opacity: 0;"">'+
+                          '<input type="checkbox" class="flat" name="table_records" value="'+row.id+'" style="position: relative; opacity: 0;">'+
                           '<ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;"></ins>'+
                          '</div>'+
                     '</td>'+
